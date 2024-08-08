32d5e798292b7d813d1ae6944b9b6e0a8f21a28f
Dharmender-Humanpixel <122670703+Aniketsharma1203@users.noreply.github.com>
2024-08-08 09:56:55 +0530

    Made slider by myself.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,67 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import Design from "./Design.jsx";
 import "../styles/Home.css";
 import Watch from "../utils/watch.png";
 import Shoes from "../utils/shoes.png";
 import Shirt from "../utils/shirts.png";
-import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
 
 export const Home = () => {
-  return (
-    <>
-      <div className="design-container">
-        <AwesomeSlider>
-          <div className="template">
-            <Design
-              img={Watch}
-              name={"Rolex"}
-              description={
-                <>
-                  <span>Discover timeless elegance with the Rolex</span>
-                  <br />
-                  <span>Perfect for any occasion,attending a event.</span>
-                </>
-              }
-              price={60}
-              rating={3}
-            />
-          </div>
+  const [state, setState] = useState(0);
 
-          <div className="template">
-            <Design
-              img={Shoes}
-              name={"NIKE AIR MAX"}
-              description={
-                <>
-                  <span>Experience unparalleled comfort and style.</span>
-                  <br />
-                  <span>Perfect for running, casual outings</span>
-                </>
-              }
-              price={90}
-              rating={5}
-              bgColor={"#FB552F"}
-            />
-          </div>
-          <div className="template">
-            <Design
-              img={Shirt}
-              name={"DENIM SHIRT"}
-              description={
-                <>
-                  <span>Experience unparalleled comfort and style.</span>
-                  <br />
-                  <span>Perfect for running, casual outings</span>
-                </>
-              }
-              price={30}
-              rating={2}
-              bgColor={"#52798A"}
-            />
-          </div>
-        </AwesomeSlider>
-      </div>
-    </>
+  const handleNext = () => {
+    setState((prev) => (prev + 1) % products.length);
+  };
+
+  const handlePrev = () => {
+    setState((prev) => (prev - 1 + products.length) % products.length);
+  };
+
+  const products = [
+    {
+      img: Watch,
+      name: "Rolex",
+      description: (
+        <>
+          <span>Discover timeless elegance with the Rolex</span>
+          <br />
+          <span>Perfect for any occasion, attending an event.</span>
+        </>
+      ),
+      price: 60,
+      rating: 3,
+      bgColor: "",
+    },
+    {
+      img: Shoes,
+      name: "NIKE AIR MAX",
+      description: (
+        <>
+          <span>Experience unparalleled comfort and style.</span>
+          <br />
+          <span>Perfect for running, casual outings</span>
+        </>
+      ),
+      price: 90,
+      rating: 5,
+      bgColor: "#FB552F",
+    },
+    {
+      img: Shirt,
+      name: "DENIM SHIRT",
+      description: (
+        <>
+          <span>Experience unparalleled comfort and style.</span>
+          <br />
+          <span>Perfect for running, casual outings</span>
+        </>
+      ),
+      price: 30,
+      rating: 2,
+      bgColor: "#52798A",
+    },
+  ];
+
+  return (
+    <div className="design-container">
+      {
+        <div className="template">
+          <Design
+            img={products[state].img}
+            name={products[state].name}
+            description={products[state].description}
+            price={products[state].price}
+            rating={products[state].rating}
+            bgColor={products[state].bgColor}
+          />
+        </div>
+      }
+      <button onClick={handleNext}>+</button>
+      <button onClick={handlePrev}>-</button>
+    </div>
   );
 };
